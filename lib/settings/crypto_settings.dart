bbc770d039109443f4c1496b49b835bd9f672e8e
Rafael WMS <rafael.wms@live.com>
2026-05-29 14:59:02 -0300

    gitignore e chaves

---
+++ lib/settings/crypto_settings.dart
@@ -0,0 +1,7 @@
+class CryptoSettings {
+  static const String cryptoEmailKey = 'ABCD1234EF567890ABCD1234EF567890'; // 32 chars for AES-256
+  static const String cryptoPassKey = 'ABCD1234EF567890ABCD1234EF567890';
+  static const String cryptoIpAddressKey = 'ABCD1234EF567890ABCD1234EF567890';
+}
+// AO BAIXAR RODE O COMANDO ABAIXO ANTES DE COLOCAR AS CHAVES REAIS.
+//git update-index --no-skip-worktree lib/settings/crypto_settings.dart
