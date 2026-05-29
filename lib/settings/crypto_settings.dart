0e6260b0784c7f41ff3e60ddbf0b655f27997c98
Rafael WMS <rafael.wms@live.com>
2026-05-29 14:47:32 -0300

    chore: removendo crypto_settings.dart do rastreamento do git

--- lib/settings/crypto_settings.dart
+++
@@ -1,5 +0,0 @@
-class CryptoSettings {
-  static const String cryptoEmailKey = 'ABCD1234EF567890ABCD1234EF567890'; // 32 chars for AES-256
-  static const String cryptoPassKey = 'ABCD1234EF567890ABCD1234EF567890';
-  static const String cryptoIpAddressKey = 'ABCD1234EF567890ABCD1234EF567890';
-}
